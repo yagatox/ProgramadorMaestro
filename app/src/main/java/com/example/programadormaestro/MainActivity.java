d8eae95a29934a58e0d82a5e8075b14256d81ee7
yagatox <alejandro.bucarei@gmail.com>
2021-06-18 13:07:48 -0400

    0.01

---
+++ app/src/main/java/com/example/programadormaestro/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.programadormaestro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+}
